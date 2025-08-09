83305c85972d80e2e624b5e89b244ade68ee201a
Atirek Pothiwala <atirekpothiwala@gmail.com>
2025-08-10 01:17:10 +0530

    Initial Commit

---
+++ rock-paper-scissors/rock_paper_scissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  rock_paper_scissorsApp.swift
+//  rock-paper-scissors
+//
+//  Created by Atirek Pothiwala on 10/08/25.
+//
+
+import SwiftUI
+
+@main
+struct rock_paper_scissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
